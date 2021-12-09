1bde58a51546b34d0a9fbb403d84083e96580da8
ImKrishnaMadala <88415843+ImKrishnaMadala@users.noreply.github.com>
2021-12-08 23:52:18 -0500

    commit

--- app.js
+++ app.js
@@ -6,10 +6,24 @@ function recommend() {
   
   
   var myRecommendations = [
-    "Apple Juice",
-    "Orange Juice",
-    "Strawberry Juice",
-    "Beet Juice",
-    "Pineapple Juice"
+    "Tequila sunrise,orange juice,tequila,grenadine",
+    "rustaveli,chacha,vermouth,cynar,absinthe",
+    "burrata cocktail,gin,lemon juice,gomme syrup,burrata water",
+    "ramos gin fizz,gin,orange blossom water,egg white,half and half,lemon juice,lime juice,simple syrup,ice,seltzer",
+    "mr. pink,rum,lime juice,campari,pineapple juice,simple syrup,egg white",
+    "bee to the blossom,gin,lemon,honey syrup,yellow chartreuse,orange bitters",
+    "spring fling,tequila,grapefruit juice,lime juice,orgeat,simple syrup,peychaud's bitters",
+    "fly french 75,gin,cava,lemon juice,simple syrup",
+    "strawberry fields,tequila,lime juice,agave syrup,strawberry,basil",
+    "southern revival,whiskey,passionfruit puree,simple syrup,lemon juice,basil,ginger ale",
+    "mastro's lavender collins,lavender gin,triple sec,lavender syrup,lemon juice",
+    "ladybug,brandy,lime,orgeat,dale gegroff bitters",
+    "easter egg hunt martini,vanilla vodka,triple sec,milk,orange juice,egg white",
+    "what's up doc,vodka,carrot juice,lime juice,simple syrup,ginger beer",
+    "dark and stormy,rum,ginger beer",
+    "white russian,vodka,half and half,kahlua",
+    "negroni,gin,vermouth,campari",
+    "mojito,white rum,lime juice,mint,simple syrup,seltzer",
+    "caipirinha,cachaca,lime,sugar"
   ];
   

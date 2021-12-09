69aafd0090f53c153396bfef38909d19f13cc5ce
ImKrishnaMadala <88415843+ImKrishnaMadala@users.noreply.github.com>
2021-12-09 02:15:14 -0500

    skeleton quiz selector

---
+++ static/jsfile.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+    $( ".js-click" ).click(function() {
+      $( ".js-click" ).css('background', 'green');
+    })})
--- test.py
+++ test.py
@@ -2,9 +2,6 @@ import cocktaildb
 import asyncio 
 import aiohttp
 
-
-import cocktaildb
-
 # This is in literally a list, not in the form of a dict or string
 async def list_of_categories():
   return await cocktaildb.categories()

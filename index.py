6401c30ab5f039e35841a000d8d0c2f6c252f92a
ImKrishnaMadala <88415843+ImKrishnaMadala@users.noreply.github.com>
2021-12-09 00:14:47 -0500

    Margarita instructions display test

--- index.py
+++ index.py
@@ -1,12 +1,23 @@
 from flask import Flask
 from flask import render_template,request
+import requests 
+import json 
+import webbrowser
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder="templates")
 
 
-@app.route("/")
-def root():
-    return render_template("index.html")
+@app.get("/")
+def index():
+    f = r"https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+    data = requests.get(f)
+    tt = json.loads(data.text)
+    #webbrowser.open(tt["url"])
+    
+    for i in (tt["drinks"]):
+        print(i, "\n")
+    print(len(tt["drinks"]))    
+    return render_template("index.html",tt=tt,data=data)
 
 
 

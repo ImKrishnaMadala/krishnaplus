7da665a4ebec5c7519fe3a95500292eb91b01893
ImKrishnaMadala <88415843+ImKrishnaMadala@users.noreply.github.com>
2021-12-10 14:20:14 -0500

    new commits

--- static/basic.js
+++ static/basic.js
@@ -13,7 +13,7 @@ timeInSecs--;
 }
 else {
 clearInterval(ticker);
-startTimer(5*60); // 4 minutes in seconds
+startTimer(60); // 4 minutes in seconds
 }
 
 var mins = Math.floor(secs/60);
@@ -23,6 +23,6 @@ var pretty = ( (mins < 10) ? "0" : "" ) + mins + ":" + ( (secs < 10) ? "0" : "" 
 document.getElementById("countdown").innerHTML = pretty;
 }
 
-startTimer(5*60); // 4 minutes in seconds
+startTimer(60); // 4 minutes in seconds
 
 //Credits to Gulzaib from Pakistan
